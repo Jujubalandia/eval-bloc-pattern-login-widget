5950281e9a3e9a48b7c4f4eefc2e7a0133d97e8e
Bruno Moura <bmoura@produban.com.br>
2018-10-02 06:35:34 -0300

    refactoring getters  in Bloc class

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -2,14 +2,14 @@ import 'dart:async';
 
 class Bloc {
 
-  final emailController = StreamController<String>();
+  final _emailController = StreamController<String>();
   final passwordController = StreamController<String>();
 
   // Add data to stream 
-  Function(String) get changeEmail => emailController.sink.add;
+  Function(String) get changeEmail => _emailController.sink.add;
 
   Function(String) get changePassword => passwordController.sink.add;
 
-  Stream<String> get email => emailController.stream;
+  Stream<String> get email => _emailController.stream;
   Stream<String> get password => passwordController.stream;
 }
