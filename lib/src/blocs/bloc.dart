3a78e41894060e8df61674fa96bcd69cb88c8695
Bruno Moura <bmoura@produban.com.br>
2018-10-02 06:04:03 -0300

    refactoring Add getters

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -5,4 +5,11 @@ class Bloc {
   final emailController = StreamController<String>();
   final passwordController = StreamController<String>();
 
+  // Add data to stream 
+  Function(String) get changeEmail => emailController.sink.add;
+
+  Function(String) get changePassword => passwordController.sink.add;
+
+  Stream<String> get email => emailController.stream;
+  Stream<String> get password => passwordController.stream;
 }
