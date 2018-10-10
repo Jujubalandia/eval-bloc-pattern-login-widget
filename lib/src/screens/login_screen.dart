b8f26798634b2ff0a340844676fdd7f4e454ff85
Bruno Moura <bmoura@produban.com.br>
2018-10-10 05:16:55 -0300

    Refactored  login screen for bloc

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
-final bloc = new Bloc();
+
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
           Container(margin: EdgeInsets.only(top: 25.0)),
-          emailField(),
+          emailField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
-          passwordField(),
+          passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
           submitButton(),
         ],
@@ -21,7 +25,7 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-Widget emailField() {
+Widget emailField(Bloc bloc) {
   return StreamBuilder(
     stream: bloc.email,
     builder: (context, snapshot) {
@@ -38,7 +42,7 @@ Widget emailField() {
   );
 }
 
-Widget passwordField() {
+Widget passwordField(Bloc bloc) {
   return StreamBuilder(
     stream: bloc.password,
     builder: (context, snapshot) {
